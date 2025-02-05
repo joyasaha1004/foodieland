103e0843e45df342eacf86641d0772d6f0b6e26f
joyasaha1004 <joyasaha440@gmail.com>
2025-02-05 11:30:15 -0600

    add all changes

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -116,7 +116,7 @@ const Blog = () => {
                  <h3 className="text-lg font-semibold text-stone sm:text-xl lg:text-3xl xl:text-4xl xxl:text-5xl xl:mt-2 xxl:mt-4">Know More About The Article</h3>
                 </div>
 
-                <div className="w-52 justify-self-center sm:w-56 md:w-[465px] lg:w-[600px] xl:mt-12 xl:w-[1000px]  xxl:w-[1220px]">
+                <div className="w-52 justify-self-center sm:w-60 md:w-[465px] lg:w-[600px] xl:mt-12 xl:w-[1000px]  xxl:w-[1220px]">
                 
                 <Slider ref={slider => {
           sliderRef = slider;
@@ -144,7 +144,7 @@ const Blog = () => {
                   <hr classNamE=""></hr>
                   <div className="flex xxl:pt-2">
                    <p className="text-xs text-amber mt-1 underline underline-offset-3 cursor-pointer sm:text-[9px] xl:text-[12px] xxl:text-[16px]">Read More</p>
-                   <div className="flex mt-2 ml-12 cursor-pointer md:ml-20 lg:ml-28 xl:ml-24 xxl:ml-32 ">
+                   <div className="flex mt-2 ml-12 cursor-pointer sm:ml-20 md:ml-20 lg:ml-28 xl:ml-24 xxl:ml-32 ">
                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 sm:size-3 xxl:size-5">
   <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
 </svg>
